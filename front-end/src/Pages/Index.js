7ba165976352f26906125b36cdd6cd70052a48c2
Mason Mei <masonmei@pursuit.org>
2022-08-22 17:50:57 -0500

    added index

--- front-end/src/Pages/Index.js
+++ front-end/src/Pages/Index.js
@@ -1,9 +1,17 @@
 import Snacks from "../Components/Snacks";
+import heartRegular from "../assets/heart-regular.png"
+import heartSolid from "../assets/heart-solid.png"
 
 function Index() {
   return (
     <div className="Index">
       <h2>Snack List</h2>
+      <h5>Healthy: 
+      <img src={heartSolid} height="30px" width="30px"></img>
+      </h5>
+      <h5>Unhealthy:  
+      <img src={heartRegular} height="30px" width="30px"></img>
+      </h5>
       <Snacks />
     </div>
   );
